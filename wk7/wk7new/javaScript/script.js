d6ef8ce9349d06941d2f945cbd8cbfa4e254ce35
laurinamadden <92827801+laurinamadden@users.noreply.github.com>
2021-11-21 20:33:52 +0000

    Update script.js
    Had uploaded wrong wk7 file so edited to copy in correct file

--- wk7/wk7new/javaScript/script.js
+++ wk7/wk7new/javaScript/script.js
@@ -1,11 +1,12 @@
 function output(){
+	<!--stop page from refreshing-->
+	event.preventDefault();
+	<!--hide the form-->
+	document.getElementById("hideForm").style.display = "none";
+	<!--get name and email from input on html--> 
+	var name=document.getElementById("formName").value;
+	var email=document.getElementById("formEmail").value;
 	<!--[insert user name], thank you for your details.--> 
 	<!--We will be in touch via [insert user email] shortly-->
-	document.getElementById("name").innerHTML+=name+=", thank you for your details. ";
-	document.getElementById("email").innerHTML+="We will be in touch via "+=email+=" shortly.";
-	hideForm()
+	alert(name+", thank you for your details. We will be in touch via "+email+" shortly");
 }	
-
-function hideForm(){
-	document.getElementById("hide").style/style.css.display = "none";
-}
